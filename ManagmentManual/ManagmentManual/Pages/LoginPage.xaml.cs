5bad6cd99d5158f3b9a0ccfe17b5eb5351808641
VolodymyrKor <ramon2921@gmail.com>
2017-10-10 00:53:04 +0300

    Creating login page

--- ManagmentManual/ManagmentManual/Pages/LoginPage.xaml.cs
+++ ManagmentManual/ManagmentManual/Pages/LoginPage.xaml.cs
@@ -16,16 +16,13 @@ using System.Windows.Shapes;
 namespace ManagmentManual
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginPage : Page
     {
-        public MainWindow()
+        public LoginPage()
         {
             InitializeComponent();
-
-            LoginPage loginPage = new LoginPage();
-            MainFrame.Navigate(loginPage);
         }
     }
 }
