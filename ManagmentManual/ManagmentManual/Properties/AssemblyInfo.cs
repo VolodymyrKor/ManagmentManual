01322a41c3103f42b6fae3af9a9614b009430c95
VolodymyrKor <ramon2921@gmail.com>
2017-10-18 01:20:26 +0300

    Finished working with login page (need some fixes in the future)

--- ManagmentManual/ManagmentManual/Properties/AssemblyInfo.cs
+++ ManagmentManual/ManagmentManual/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("ManagmentManual")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ManagmentManualCorp")]
 [assembly: AssemblyProduct("ManagmentManual")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
