88fd66709e9f1094a0fdd930eb548bb757be46b9
VolodymyrKor <ramon2921@gmail.com>
2017-10-21 00:44:28 +0300

    Creating registration page with correct working registration

--- ManagmentManual/ManagmentManual/Pages/RegistrationPage.xaml.cs
+++ ManagmentManual/ManagmentManual/Pages/RegistrationPage.xaml.cs
@@ -24,5 +24,57 @@ namespace ManagmentManual.Pages
         {
             InitializeComponent();
         }
+
+        private void RegisterStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (StudentPasswordTextBox.Password != StudentPassword2TextBox.Password)
+                {
+                    throw new Exception("Passwords are not equal!");
+                }
+                if (MainWindow.DB_DATA.Users.Any(user => user.USER_EMAIL == StudentEmailTextBox.Text))
+                {
+                    throw new Exception("User with this email already exists!");
+                }
+
+                MainWindow.DB_DATA.Users.Add(new User()
+                {
+                    USER_NAME = StudentNameTextBox.Text,
+                    USER_SURNAME = StudentSurnameTextBox.Text,
+                    USER_MIDDLE_NAME = StudentMiddleNameTextBox.Text,
+                    USER_EMAIL = StudentEmailTextBox.Text,
+                    USER_PASSWORD = StudentPasswordTextBox.Password,
+                    // todo [VK]: check on null pointer
+                    // todo [VK]: why too big id???
+                    USER_TYPE_ID = MainWindow.DB_DATA.UserTypes.FirstOrDefault(type => type.TYPE_NAME == "student").TYPE_ID
+                });
+
+                if (MainWindow.DB_DATA.SaveChanges() > 0)
+                {
+                    MainWindow.CURRENT_USER_ID = MainWindow.DB_DATA.Users
+                        .FirstOrDefault(user => user.USER_EMAIL == StudentEmailTextBox.Text).USER_ID;
+
+                    NavigationService?.Navigate(new StartStudentPage());
+                }
+                else
+                {
+                    throw new Exception("Problem with registration. Please try again.");    
+                }
+            }
+            catch (NullReferenceException exception)
+            {
+                MessageBox.Show(exception.Message + "\n" + "Some problems with database(((", "Error");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        private void BackToStartPage_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.GoBack();
+        }
     }
 }
