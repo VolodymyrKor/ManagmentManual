f788669289069c265295cb933e2229c866dfcecd
Zoriana Yaremko <zoryana36@gmail.com>
2017-11-01 21:10:34 +0200

    Expert pages added

---
+++ ManagmentManual/ManagmentManual/Pages/TestViewingExpertPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManagmentManual.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TestViewingExpertPage.xaml
+    /// </summary>
+    public partial class TestViewingExpertPage : Page
+    {
+        public TestViewingExpertPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var service = NavigationService.GetNavigationService(this);
+            service.Navigate(new StartExpertPage());
+        }
+    }
+}
