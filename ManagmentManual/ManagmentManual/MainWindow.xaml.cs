0c75aa31b15cfe48fbbcf21c31c66f86bca7105b
Zoriana Yaremko <zoryana36@gmail.com>
2017-11-02 10:46:19 +0200

    Added ages

--- ManagmentManual/ManagmentManual/MainWindow.xaml.cs
+++ ManagmentManual/ManagmentManual/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace ManagmentManual
         {
             InitializeComponent();
 
-            MainFrame.NavigationService.Navigate(new LoginPage());
+            MainFrame.NavigationService.Navigate(new StartExpertPage());
         }
     }
 }
